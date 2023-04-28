64e361bc14548132912e3c319e2eedb2011a2cbd
AntonAhlm <113014145+AntonAhlm@users.noreply.github.com>
2023-04-28 08:58:14 +0200

    Update Program.cs

--- Black Jack/Program.cs
+++ Black Jack/Program.cs
@@ -6,83 +6,128 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            bool Avsluta = false;
+            bool Avsluta = false; //Loop-villkor för hela programmet. 
             Random slump=new Random();
             int SpelarePoäng = 0;
-            int DatorPoäng = 0; 
-            while(Avsluta!=true)
+            int DatorPoäng = 0;
+            string SenasteVinnare ="";
+            while(Avsluta!=true) //Loop för hela progammet.
             { 
-            Console.WriteLine("Välkommen til 21:an! \n" +
+            Console.WriteLine("Välkommen til 21:an! \n" + //Menyn
                 "Välj ett alternativ      \n " +
                 "1. Spela 21:an           \n " +
                 "2. Visa senaste vinnaren \n " +
                 "3. Spelets regler        \n " +
-                "4. Avsluta programmet");
+                "4. Avsluta programmet    \n ");
 
             string val=Console.ReadLine();
+             
 
                 switch (val)
                 {
                     case "1":
 
-                        SpelarePoäng = 0;
+                        SpelarePoäng = 0; //Nollar poängen
                         DatorPoäng= 0;
 
+                        SpelarePoäng = SpelarePoäng + slump.Next(1, 11); //Drar 2 kort till dig 
                         SpelarePoäng = SpelarePoäng + slump.Next(1, 11);
-                        SpelarePoäng = SpelarePoäng + slump.Next(1, 11);
-                        DatorPoäng = DatorPoäng + slump.Next(1, 11);
-                        DatorPoäng = DatorPoäng + slump.Next(1, 11);
-                        Console.WriteLine("Du har " + SpelarePoäng + " poäng");
-                        Console.WriteLine("Datorn har " + DatorPoäng + " poäng");
-                        string svar = "";
 
-                        do
+                        DatorPoäng = DatorPoäng + slump.Next(1, 11); //Drar 2 kort till datorn
+                        DatorPoäng = DatorPoäng + slump.Next(1, 11);
+
+                       
+                        Console.WriteLine("Du drog 2 kort och har " + SpelarePoäng + " poäng"); //Skriver ut hur många poäng du och datorn har
+                        Console.WriteLine("Datorn drog 2 kort och har " + DatorPoäng + " poäng");
+                        string svar = "j";
+                        
+
+                        while (svar != "n")
                         {
-                            Console.WriteLine("Vill du dra att till kort? j/n");
+                            Console.WriteLine("Vill du dra att till kort? j/n"); //Frågar om du vill dra ett kort 
                             svar = Console.ReadLine();
-                            if (svar == "j") ;
+
+                            if (svar != "n" && SpelarePoäng<=21) //Om Svaret inte är n och du inte har över 21 poäng:
                             {
                                 SpelarePoäng = SpelarePoäng + slump.Next(1, 11);
-                                Console.WriteLine("Du har " + SpelarePoäng + " poäng");
-                                Console.WriteLine("Datorn har " + DatorPoäng + " poäng");
-                                
+                                Console.WriteLine("Du drog ett kort och har " + SpelarePoäng + " poäng");
+                                Console.WriteLine("Datorn har " + DatorPoäng + " poäng \n");
+
 
                             }
 
-                            if (SpelarePoäng > 21)
+                            if (SpelarePoäng > 21) //Om du har över 21 poäng
                             {
                                 svar = "n";
                                 DatorPoäng = DatorPoäng + slump.Next(1, 11);
+                                Console.WriteLine("Datorn drog ett kort och har " + DatorPoäng + " poäng \n");
                             }
 
+                            else if (svar == "n") //Eller om Svaret är nej
+                            {
+                                svar = "n";
+                                DatorPoäng = DatorPoäng + slump.Next(1, 11);
+                                Console.WriteLine("Datorn drog ett kort och har " + DatorPoäng + " poäng \n");
 
-                        } while (svar != "n");
 
-                        if (SpelarePoäng > DatorPoäng && SpelarePoäng <= 21)
+                            }
+                            
+                        }
+
+                        Console.WriteLine("Du har " + SpelarePoäng + " poäng"); //Skriver hur många poäng spelarna har
+                        Console.WriteLine("Datorn har " + DatorPoäng + " poäng");
+
+                        if (SpelarePoäng > DatorPoäng && SpelarePoäng <= 21) // Om du har mer än datorn och under 21 : vinst
+                        {
                             Console.WriteLine("Du Vann!");
+                            SenasteVinnare = "Du";
+                        }
 
-                        else if (SpelarePoäng <= 21 && DatorPoäng <= 21 && DatorPoäng > SpelarePoäng)
+                        else if (SpelarePoäng <= 21 && DatorPoäng <= 21 && DatorPoäng > SpelarePoäng) //Om båda har under 22 och datorn har mer än du : förlust
+                        {
                             Console.WriteLine("Du Förlorade!!");
+                            SenasteVinnare = "Datorn";
+                        }
 
-                        else if (SpelarePoäng > 21 && DatorPoäng < 21)
+                        else if (SpelarePoäng > 21 && DatorPoäng < 21)  //Om du har över 21 och datorn under 21 : förlust
+                        {
                             Console.WriteLine("Du Förlorade!!");
+                            SenasteVinnare = "Datorn";
+                        }
 
-                        else if (DatorPoäng <21 && SpelarePoäng >21 )
-                            Console.WriteLine("Du Förlorade!!");
-
-                        else if (SpelarePoäng > 21 && DatorPoäng > 21)
+                        else if (SpelarePoäng > 21 && DatorPoäng > 21) //Om båda har över 21: ingen vann
                             Console.WriteLine("Ingen Vann!");
-                        else
-                            Console.WriteLine("Skit i det");
+
+                        else if (SpelarePoäng > 21) //Om du har över 21 poäng: förlust
+                        {
+                            Console.WriteLine("Du Förlorade!!");
+                            SenasteVinnare = "Datorn";
+                        }
+
+                        else //Testkod ifall det inte skriver ut något annat
+                            Console.WriteLine(" :( ????????");
+
+                        Thread.Sleep(2000);
 
                         Console.WriteLine("\n");
                         break;
 
-                    case "2":
-                        Console.WriteLine();
+                    case "2": //Senaste vinnaren
+
+                        if(SenasteVinnare=="") //Ingen vinnare / Gör ingenting
+                        {
+                            break;
+                        }
+
+                        else  //Skriver ut vinnaren
+                        {
+                            Console.WriteLine("Senaste vinnaren är : " + SenasteVinnare + "\n");
+                        }
+
+                        Thread.Sleep(2000);
                         break;
 
-                    case "3":
+                    case "3": //Reglerna
                         Console.WriteLine(" \n Ditt mål är att tvinga datorn att få mer än 21 poäng, \n" +
                             "Du får poäng genom att dra kort, varje kort har 1-10 poäng. \n" +
                             "Om du får mer än 21 poäng har du förlorat. \n" +
@@ -93,7 +138,7 @@ namespace Program
                         Thread.Sleep(4000);
                         break;
 
-                    case "4":
+                    case "4": //Avslutar
                         Avsluta = true;
                         break;
                         
